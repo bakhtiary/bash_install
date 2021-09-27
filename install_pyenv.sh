625c53655c3bd3a09769962073142e216867c6d0
amir <amir.h.bakhtiary@gmail.com>
2021-09-27 16:42:26 +0200

    script for installing pyenv in ubuntu

---
+++ install_pyenv.sh
@@ -0,0 +1,24 @@
+sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+
+cat <<EOT >> ~/.bashrc
+## pyenv configs
+export PYENV_ROOT="\$HOME/.pyenv"
+export PATH="\$PYENV_ROOT/bin:\$PATH"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "\$(pyenv init -)"
+fi
+EOT
+
+cat <<EOT >> ~/.zshrc
+## pyenv configs
+export PYENV_ROOT="\$HOME/.pyenv"
+export PATH="\$PYENV_ROOT/bin:\$PATH"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "\$(pyenv init -)"
+fi
+EOT
+
+
